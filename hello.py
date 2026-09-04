98adeb6af451a600d93613455913f63a4b487636
Joshua <2603053@sit.singaporetech.edu.sg>
2026-09-04 16:37:58 +0800

    growth_tracker

--- hello.py
+++ hello.py
@@ -13,3 +13,16 @@ followers= 100
 print("Username:", username)
 print("Bio:", bio)
 print("Follwers:", followers)
+
+#activity 3 (growth_tracker)
+
+followers = 100
+
+followers +=50
+print("Day1:", followers)
+
+followers += 20
+print("Day2:", followers)
+
+followers -= 50
+print("Day3:", followers)

f96c366cee9538bde615ff64b0674bd13de1acd3
Joshua <2603053@sit.singaporetech.edu.sg>
2026-09-04 16:40:06 +0800

    interective_profile

--- hello.py
+++ hello.py
@@ -26,3 +26,18 @@ print("Day2:", followers)
 
 followers -= 50
 print("Day3:", followers)
+
+#activity 4 "interective_profile"
+
+username= input("Enter Username: ")
+age= int(input("Enter Age: "))
+category=input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")

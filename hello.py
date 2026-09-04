245c44636e310ca64a7217b9929941a1be59bded
Joshua <2603053@sit.singaporetech.edu.sg>
2026-09-04 16:32:43 +0800

    First Post

--- hello.py
+++ hello.py
@@ -2,36 +2,3 @@ print("===========================")
 print("Welcome here")
 print("My first post!")
 print("===========================")
-
-username = "cool_creator"
-bio= "Fun Blogger"
-followers= 1000
-
-print("Username:", username)
-print("Bio:", bio)
-print("Follwers:", followers)
-
-followers = 100
-
-followers +=50
-print("Day1:", followers)
-
-followers += 20
-print("Day2:", followers)
-
-followers -= 50
-print("Day3:", followers)
-
-username= input("Enter Username: ")
-age= int(input("Enter Age: "))
-category=input("Enter Content Category: ")
-
-print("\nInstagram Profile")
-print("=====================")
-print("Username:", username)
-print("Age:", age)
-print("Category:", category)
-
-if age>40 and category == "fun":
-    print("You are old what is fun for you??")
-
